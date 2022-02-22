d57e502928925120a18dbbb44f138a431f4cd4f0
alibaevauulzhan <97526174+alibaevauulzhan@users.noreply.github.com>
2022-02-22 20:53:59 +0600

    fixed bugs

--- MassMarket/settings.py
+++ MassMarket/settings.py
@@ -117,7 +117,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 1
 
 }
 from datetime import timedelta
